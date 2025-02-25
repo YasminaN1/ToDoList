98bee4d5d1a410d9197419f682daa70b49de0deb
YasminaN1 <526ynigmanova@frhsd.com>
2025-02-25 09:29:47 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Init(Create) an empty array element called tasks
+let tasks = [];
+
+//Add an ON CLICK event listener to the "add task button" that calls a function
+document.getElementById('addTaskBtn').addEventListener('click', function() {
+//Get value of input box and store it in a variable called taskInput
+let taskInput = document.getElementById('taskInput').ariaValueMax
+
+//Check if taskInput has value or smth in it
+if(taskInput)
+});
