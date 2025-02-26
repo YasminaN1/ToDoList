d3f8e41591df2d05342edf55da7d88bb62a156cc
YasminaN1 <526ynigmanova@frhsd.com>
2025-02-26 11:51:45 -0500

    need to add clear button

--- script.js
+++ script.js
@@ -2,10 +2,47 @@
 let tasks = [];
 
 //Add an ON CLICK event listener to the "add task button" that calls a function
-document.getElementById('addTaskBtn').addEventListener('click', function() {
-//Get value of input box and store it in a variable called taskInput
-let taskInput = document.getElementById('taskInput').ariaValueMax
+document.getElementById("addTaskBtn").addEventListener("click", function () {
+  //Get value of input box and store it in a variable called taskInput
+  let taskInput = document.getElementById("taskInput").value;
 
-//Check if taskInput has value or smth in it
-if(taskInput)
+  //Check if taskInput has value or smth in it
+  if (taskInput) {
+    // Add the new tasks to the task array
+    tasks.push(taskInput);
+
+    //Clear the input field after adding the task
+    document.getElementById("taskInput").value = "";
+
+    //Call the function to update the task list display
+    displayTasks();
+  }
 });
+
+//A function that will display all tasks into lists
+function displayTasks() {
+  //Select the unordered list (tasklist) in the HTML
+  let taskList = document.getElementById("taskList");
+
+  //Clear the existing task list before updating it
+  taskList.innerHTML = "";
+
+  //Loop through every task in the array and create a list item
+  tasks.forEach((task, index) => {
+    // create a new <li> element for each task
+    let li = document.createElement("li");
+
+    //Add CSS classes for styling
+    li.classList.add(
+      "list-group-item",
+      "d-flex",
+      "justify-content-between",
+      "align-items-center"
+    );
+
+    li.innerHTML = `${task} <button class='btn btn-dark btn-sm' onclick = 'removeTask(${index}'> √ </button>`;
+
+    //Append  the new task to the task list
+    taskList.appendChild(li);
+  });
+}
